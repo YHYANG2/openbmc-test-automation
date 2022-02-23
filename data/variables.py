c6b96461d30bf90b49fc4d9a26679376ad1f7927
Tim Lee <timlee660101@gmail.com>
2022-02-23 10:53:19 +0800

    data: variables: change OperatingSystem status variables
    Symptom:
    Standby != xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby
    
    Root cause:
    We have synced to latest x86-power-control with our commit.
    https://github.com/openbmc/x86-power-control/commit/8623918b9528cc234dcefe32b83f07ad73a49145
    However, variables OS_BOOT_COMPLETE didn't sync to it.
    
    Soluction:
    Variables OS_BOOT_COMPLETE change back to "OperatingSystem.Status.OSStatus.Standby".
    
    Signed-off-by: Tim Lee <timlee660101@gmail.com>

--- data/variables.py
+++ data/variables.py
@@ -122,7 +122,8 @@ OS_BOOT_MOTHERBOARD = STATE_DBUS_BASE + \
     'Boot.Progress.ProgressStages.MotherboardInit'
 
 # OperatingSystem status variables.
-OS_BOOT_COMPLETE = 'Standby'
+OS_BOOT_COMPLETE = STATE_DBUS_BASE + \
+    'OperatingSystem.Status.OSStatus.Standby'
 OS_BOOT_CDROM = STATE_DBUS_BASE + 'OperatingSystem.Status.OSStatus.CDROMBoot'
 OS_BOOT_ROM = STATE_DBUS_BASE + 'OperatingSystem.Status.OSStatus.ROMBoot'
 OS_BOOT_PXE = STATE_DBUS_BASE + 'OperatingSystem.Status.OSStatus.PXEBoot'
